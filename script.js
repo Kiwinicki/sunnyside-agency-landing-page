cb3871c82bec709266cb463cbf5f334153d9495d
DESKTOP-2P9DAPD\Windows 10 <j.kiwinicki@wp.pl>
2021-10-13 20:26:57 +0200

    added README and screens

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 (() => {
 	const headerMenu = document.querySelector('.site-header__nav-menu');
 	document.addEventListener('resize', () => {
-		document.body.clientWidth >= 600 ? headerMenu.classList.remove('site-header__nav-menu--hidden') : headerMenu.classList.add('site-header__nav-menu--hidden');
+		document.body.clientWidth >= 1024 ? headerMenu.classList.remove('site-header__nav-menu--hidden') : headerMenu.classList.add('site-header__nav-menu--hidden');
 	});
 
 	const btnMenu = document.querySelector('.site-header__nav-btn');
